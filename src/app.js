84e46dae5598c736cb3745e87d8f235f479743f5
Tanner Stephens <tannerrstephens@gmail.com>
2020-05-13 00:11:14 -0500

    Remove renderMap function

--- src/app.js
+++ src/app.js
@@ -92,37 +92,6 @@ class App {
 
     render();
   }
-
-  async renderMap() {
-
-
-    // const imageData = this.context.createImageData(innerWidth, innerHeight);
-
-    // const setPixel = (x, y, color) => {
-    //   const index = (y*imageData.width + x)*4;
-
-    //   const r = (color & 0xff0000) >> 16;
-    //   const g = (color & 0x00ff00) >> 8;
-    //   const b = (color & 0x0000ff);
-
-    //   imageData.data[index] = r;
-    //   imageData.data[index+1] = g;
-    //   imageData.data[index+2] = b;
-    //   imageData.data[index+3] = 255;
-    // }
-
-    // const oct1 = this.noiseGenerator.generate(0, 0, innerWidth, innerHeight, 1024);
-    // const oct2 = this.noiseGenerator.generate(0, 0, innerWidth, innerHeight, 256, 0.25);
-    // const oct3 = this.noiseGenerator.generate(0, 0, innerWidth, innerHeight, 64, 0.0625);
-
-    // oct1.forEach((column, x) => column.forEach((value, y) => {
-    //   const totalValue = value + oct2[x][y] + oct3[x][y];
-    //   const color = totalValue < 0.1 ? constants.waterColor : constants.terrainColors[Math.min(Math.floor(totalValue*constants.terrainColors.length), constants.terrainColors.length-1)]
-    //   setPixel(x, y, color);
-    // }));
-
-    // this.context.putImageData(imageData, 0, 0);
-  }
 }
 
 export default App;
